3cad9746046286027a4e038ad316b0df9dec7883
Katy <surekaty0919@gmail.com>
2022-12-14 14:33:27 +0800

    finish 10190

--- 10190Divide,ButNotQuiteConquer!/10190Divide,ButNotQuiteConquer!.py
+++ 10190Divide,ButNotQuiteConquer!/10190Divide,ButNotQuiteConquer!.py
@@ -1,5 +1,26 @@
-
 if __name__ == "__main__":
-    integer = input("Enter number that will be divided:")
-    devide = input("Enter number that will divide the former one:")
-    
+    num = 0
+    result = []
+    while num<4:
+        s1 = input("Enter number that will be divided:")
+        s2 = input("Enter number that will divide the former one:")
+        integer = int(s1)
+        devide = int(s2)
+        output = []
+        while 1:
+            output.append(integer)
+            remainder = integer % devide
+            if remainder != 0:
+                result.append('Boring!')
+                break
+            else:
+                integer = integer//devide
+                if integer == 1:
+                    output.append(integer)
+                    result.append(output)
+                    break
+        num+=1
+    print(result)
+
+        
+
