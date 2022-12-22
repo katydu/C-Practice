f2a84201685ebe3569d1d0984bad196051f02913
Katy <surekaty0919@gmail.com>
2022-12-22 17:29:50 +0800

    finish 12908

--- 12908Thebookthief/12908Thebookthief.py
+++ 12908Thebookthief/12908Thebookthief.py
@@ -1,2 +1,28 @@
+output = []
+def caluclatePage(numbers,count):
+    total = 0
+    for i in range(100000):
+        total = total + numbers[i]
+        if total >= count:
+            if total - count == 0:
+                output.append(str(numbers[i+1])+" "+str(i+1))
+                break
+            else:
+                output.append(str(total - count)+" "+str(i))
+                break
+
 if __name__ == "__main__":
-    countPages = input("Enter Anita count pages:")
+    numbers = []
+    for i in range(100000):
+        numbers.append(i)
+    while 1:
+        count = input("Enter Anita count pages:")
+        if int(count) == 0:
+            break
+        else:
+            caluclatePage(numbers,int(count))
+    for i in range(len(output)):
+        print(output[i])
+    
+    
+    
