71a953674a4933345cdc118616d029742bea126a
Katy <surekaty0919@gmail.com>
2022-12-23 16:00:13 +0800

    change parameter name

--- 12908Thebookthief/12908Thebookthief.py
+++ 12908Thebookthief/12908Thebookthief.py
@@ -1,26 +1,27 @@
 output = []
-def caluclatePage(numbers,input):
+def caluclatePage(numbers,AnitaCountPages):
     total = 0
     for i in range(100000):
         total = total + numbers[i]
-        if total >= input:
-            if total - input == 0:
-                output.append(str(numbers[i+1])+" "+str(i+1))
+        if total >= AnitaCountPages:
+            if total - AnitaCountPages == 0:
+                output.append(str(numbers[i+1])+" "+str(i+2))
                 break
             else:
-                output.append(str(total - input)+" "+str(i))
+                output.append(str(total - AnitaCountPages)+" "+str(i+1))
                 break
 
 if __name__ == "__main__":
     numbers = []
-    for i in range(100000):
+    for i in range(1,100001):
         numbers.append(i)
+    print(numbers[0])
     while 1:
-        input = input("Enter Anita count pages:")
-        if int(input) == 0:
+        AnitaCountPages = input("Enter Anita count pages:")
+        if int(AnitaCountPages) == 0:
             break
         else:
-            caluclatePage(numbers,int(input))
+            caluclatePage(numbers,int(AnitaCountPages))
     for i in range(len(output)):
         print(output[i])
     
